0449f951fbbd6fbc780a11f18e868fd0eefacb53
Leopold <leopold.pinkernell@tobit.com>
2020-09-07 17:46:02 +0200

    :construction: Add Dialog

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,32 @@ import { hot } from 'react-hot-loader/root';
 import React, { PureComponent } from 'react';
 
 class App extends PureComponent {
+
+    componentDidMount() {
+        chayns.dialog.select({
+            multiselect: true,
+            message: 'Bist du sicher, dass du *Menge*€ Trinkgeld geben willst?',
+            list: [{
+                name: 'Anonym verschicken',
+                value: 999,
+                isSelected: true,
+            }, {
+                name: 'Betrag verbergen',
+                value: {
+                    anount: 23,
+                },
+                isSelected: true,
+            }]
+        }).then((data) => {
+            console.log(data);
+        });
+    }
+
     render() {
         return (
-            <>
-                {/* ToDo: Add your components here */}
-            </>
+            <div>
+                lelelel
+            </div>
         );
     }
 }
