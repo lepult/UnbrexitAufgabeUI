b2b6b7df23b30a6dd3d329933d8040461ceb9194
Leopold <leopold.pinkernell@tobit.com>
2020-09-07 16:43:15 +0200

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import { hot } from 'react-hot-loader/root';
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+    render() {
+        return (
+            <>
+                {/* ToDo: Add your components here */}
+            </>
+        );
+    }
+}
+
+export default App;
+export const HotApp = hot(App);
